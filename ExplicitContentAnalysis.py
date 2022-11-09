b29f48db7e2f44dc1655bf75bd60941bb44db055
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-11-09 23:47:23 +0630

    Update ExplicitContentAnalysis.py

--- ExplicitContentAnalysis.py
+++ ExplicitContentAnalysis.py
@@ -8,7 +8,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2021/02/09/09/57/cassettes-5998030_1280.jpg");
+             background-image: url("https://cdn.pixabay.com/photo/2017/08/07/21/53/still-2608285_1280.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
