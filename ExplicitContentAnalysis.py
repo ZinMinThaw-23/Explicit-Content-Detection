7c55029605802984c36fbfd7c8e1e84e4a4d87b9
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-11-09 23:12:13 +0630

    Create ExplicitContentAnalysis.py

---
+++ ExplicitContentAnalysis.py
@@ -0,0 +1,64 @@
+import sys 
+
+sys.path.append('/usr/local/lib/python3.9/site-packages')
+
+import streamlit as st
+import pandas as pd
+from pyecharts.charts import Pie
+from numpy import array
+from collections import Counter
+from sklearn.cluster import KMeans
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from colorblind import colorblind
+from scipy.spatial import KDTree
+from webcolors import (
+    CSS3_HEX_TO_NAMES,
+    hex_to_rgb,
+)
+
+st.set_page_config( page_title="Color Analysis with David",page_icon="🧊",layout="wide", initial_sidebar_state="expanded")      
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url(//cdn.shopify.com/s/files/1/0011/4651/9637/t/220/assets/moonbackground.png?v=7157594…);
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()   
+
+streamlit_style = """
+			<style>
+			@import url("https://fonts.shopifycdn.com/nunito_sans/nunitosans_n7.5bd4fb9346d13afb61b3d78f8a1e9f31b128b3d9.woff2?h1=c3RvcmUudGF5bG9yc3dpZnQuY29t&hmac=60eccdbd2670eec353d0081774bf120b82d525923d9ffdd1d1f37aaf86d8bcb8") format("woff2"), url("https://fonts.shopifycdn.com/nunito_sans/nunitosans_n7.2bcf0f11aa6af91c784a857ef004bcca8c2d324d.woff?h1=c3RvcmUudGF5bG9yc3dpZnQuY29t&hmac=2bc2b91b35c84d85452727bfe0d6cc4ac43bf8978e2a27cfb29c154128a4a4cc") format("woff");
+
+			html, body, [class*="css"]  {
+			font-family: 'Nunito Sans', sans-serif;
+			font-size:25px;
+			color: 	#a1bbc5;
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:100px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">Taylor Swift Midnights Album Vinyl Varients Color Analysis with David </p>', unsafe_allow_html=True)
+
+st.image("Midnights-Logo.png", width=200)
+st.image("TS-Midnights-Logo.PNG", width=200)
