8573fab0fe47390fdb6abb3bc81fa105ab75b677
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-11-19 03:08:39 +0630

    Update Explicit_Content_Detection_in_Music_Lyrics.py

--- Explicit_Content_Detection_in_Music_Lyrics.py
+++ Explicit_Content_Detection_in_Music_Lyrics.py
@@ -90,6 +90,6 @@ if submit:
 	for i in output:
     		if(i>0.5):
         		output_string="The song includes explicit words"
-    		elseif(i<0.5):
+    		else:
         		output_string="The song is clean and doesn't include explicit words"
 	st.text_area(label="Output Data:", value=output_string, height=50)
