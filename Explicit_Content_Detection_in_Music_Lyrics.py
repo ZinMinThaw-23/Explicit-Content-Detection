291775858702bb16c8bca3a4ac6da3e02f0b73bb
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-11-19 21:48:54 +0630

    Update Explicit_Content_Detection_in_Music_Lyrics.py

--- Explicit_Content_Detection_in_Music_Lyrics.py
+++ Explicit_Content_Detection_in_Music_Lyrics.py
@@ -77,14 +77,7 @@ if submit:
 	word_index = tokenizer.word_index
 	sequences = tokenizer.texts_to_sequences(input)
 	padded = pad_sequences(sequences,maxlen=max_length, truncating=trunc_type,padding='post')
-	
-	# load json and create model
-	json_file = open('model.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model.load_weights("model.h5")
+	loaded_model = pickle.load(open("Classifier.sav", 'rb'))
 	output = loaded_model.predict(padded)
 	for i in output:
     		if(i>0.5):
