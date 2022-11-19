f15501e6e0b7c58db56f119fbdabe27fbef217d9
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-11-19 22:43:53 +0630

    Update Explicit_Content_Detection_in_Music_Lyrics.py

--- Explicit_Content_Detection_in_Music_Lyrics.py
+++ Explicit_Content_Detection_in_Music_Lyrics.py
@@ -80,8 +80,8 @@ if submit:
 	loaded_model = pickle.load(open("Classifier.sav", 'rb'))
 	output = loaded_model.predict(padded)
 	for i in output:
-    		if(i==1):
-        		output_string="The song includes explicit words"
     		if(i==0):
-        		output_string="The song is clean and doesn't include explicit words"
+        		output_string="The song doesn't include explicit words"
+    		else:
+        		output_string="The song include explicit words"
 	st.text_area(label="Output Data:", value=output_string, height=50)
